853b828a1b2d8c947e0f16d155cb8c4a038b16ba
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-03-29 23:12:47 +0300

    fix bag with closing window

--- client/src/hooks/useRedirect.js
+++ client/src/hooks/useRedirect.js
@@ -4,6 +4,9 @@ import { useNavigate } from "react-router"
 export function useRedirect() {
     const navigate = useNavigate()
     useEffect(() => {
+        if (window.history.length >= 2) {
+            window.open('', '_self').window.close();
+        }
         if ( localStorage.getItem('user') ) {
             navigate('/home')
         } else {
