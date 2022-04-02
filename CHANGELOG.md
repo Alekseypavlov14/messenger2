7349682f3ddea097dca96af3e6c87e906b70cdb7
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-04-02 19:25:30 +0300

    style changes

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+Version: 1.0.1
+Date: 29.03.2022
+New:
+1) Change some style
+
+Version: 1.0.0
+Date: 28.03.2022
+New: 
+1) Basic messenger
--- client/src/hooks/useRedirect.js
+++ client/src/hooks/useRedirect.js
@@ -3,10 +3,8 @@ import { useNavigate } from "react-router"
 
 export function useRedirect() {
     const navigate = useNavigate()
+    
     useEffect(() => {
-        if (window.history.length >= 3) {
-            window.open('', '_self').window.close();
-        }
         if ( localStorage.getItem('user') ) {
             navigate('/home')
         } else {
