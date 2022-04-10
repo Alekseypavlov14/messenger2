c68fe9aadc1cbb22a90295c7f0d58e0b8b14bb5a
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-04-10 21:48:20 +0300

    fix bag with deleting accouts

--- routes/controllers/AccountRoutesController.js
+++ routes/controllers/AccountRoutesController.js
@@ -1,5 +1,6 @@
 const User = require('./../../models/User')
 const Message = require('./../../models/Message')
+const Chat = require('../../models/Chat')
 
 class AccountRoutesController {
     async delete(req, res) {
@@ -16,6 +17,14 @@ class AccountRoutesController {
 
         outgoingMessages.forEach(message => {
             Message.findOneAndDelete(message)
+        })
+
+        const chats = await Chat.find({
+            users: user._id
+        })
+
+        chats.forEach(chat => {
+            Chat.findOneAndDelete(chat)
         })
 
         res.json({
