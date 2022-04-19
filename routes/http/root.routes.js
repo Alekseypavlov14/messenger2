d88423ebe52ca0bb473318b0f45abf97d092da2a
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-04-19 20:09:14 +0300

    add not-found route

--- routes/http/root.routes.js
+++ routes/http/root.routes.js
@@ -12,6 +12,10 @@ router.use(bodyParser.json())
 router.use(express.static(clientPath))
 router.use(express.static(path.join(clientPath, 'static')))
 
+router.use('/auth', require('./auth.routes'))
+router.use('/chat', checkUser, require('./chat.routes'))
+router.use('/account', checkUser, require('./account.routes'))
+
 router.get('/', (req, res) => RootRoutesController.index(req, res))
 router.get('/login', (req, res) => RootRoutesController.index(req, res))
 router.get('/register', (req, res) => RootRoutesController.index(req, res))
@@ -19,9 +23,6 @@ router.get('/home', (req, res) => RootRoutesController.index(req, res))
 router.get('/new-chat', (req, res) => RootRoutesController.index(req, res))
 router.get('/chat', (req, res) => RootRoutesController.index(req, res))
 router.get('/account', (req, res) => RootRoutesController.index(req, res))
-
-router.use('/auth', require('./auth.routes'))
-router.use('/chat', checkUser, require('./chat.routes'))
-router.use('/account', checkUser, require('./account.routes'))
+router.get('*', (req, res) => RootRoutesController.index(req, res))
 
 module.exports = router
